a04bab90802959921f742fc48321c7efcb4ae79f
DakshJ4033 <jaindaksh135@gmail.com>
2024-02-15 18:19:45 -0800

    Initial commit

---
+++ Aadat/AadatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AadatApp.swift
+//  Aadat
+//
+//  Created by Daksh Jain on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AadatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aadat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aadat
+//
+//  Created by Daksh Jain on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
