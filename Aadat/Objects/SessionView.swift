a1ff2a0545ed2f3b2d0434a09a071c65c48485e8
Sauvikesh <saulal@ucdavis.edu>
2024-02-19 14:07:27 -0800

    feat: added methods for session start and end times. Displaying task name(currently a placeholder value) and the start time

--- Aadat/Objects/SessionView.swift
+++ Aadat/Objects/SessionView.swift
@@ -10,20 +10,61 @@ import SwiftUI
 
 /* Session Class Dec. */
 class Session {
-    var startTime: Date?
+    var taskName: String
+    var startTime: Date
     var endTime: Date?
+    
+    // create a session for the task you want to work on
+    // constructor automatically adds the start time on creation
+    init(taskName: String = "") {
+            self.taskName = taskName
+            self.startTime = Date()
+        }
+    
+    func totalTime() -> TimeInterval {
+            return (endTime ?? Date()).timeIntervalSince(startTime)
+    }
+    
+    func endSession() {
+        self.endTime = Date()
+    }
+    
+    
+    // user friendly formatted time getters
+    func getStartTime() -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: self.startTime)
+    }
+    
+    func getEndTime() -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: self.endTime ?? Date())
+    }
+    
+    func getTotalTime() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .full
+        return formatter.string(from: self.totalTime())!
+    }
 }
 
 struct SessionView: View {
+    var session: Session
+    
     var body: some View {
-        VStack {
-
-            
+        HStack {
+            Text("\(session.taskName)")
+                .lineLimit(1)
+            Text("Started: \(session.getStartTime())")
         }
         .padding()
+        .background(.gray)
     }
 }
 
 #Preview {
-    SessionView()
+    SessionView(session: Session(taskName: "Homework"))
 }
